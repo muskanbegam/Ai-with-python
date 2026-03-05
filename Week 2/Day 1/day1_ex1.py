b27363deb1ad50d171eb471028880b3c7f16fd30
muskanbegam <mkmuskanbegam@gmail.com>
2026-03-05 16:11:21 +0530

    Week 2 Execrice 1

---
+++ Week 2/Day 1/day1_ex1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.arange(1,6)
+b = np.arange(6,11)
+
+print("*************************************")
+print(f"The arrays are \n a = {a} \n b = {b}")
+print("*************************************")
+print(f"Addition       : {a+b}")
+print(f"Subtraction    : {a-b}")
+print(f"Multiplication : {a*b}")
+print(f"Division       : {a/b}")
+print("*************************************")
